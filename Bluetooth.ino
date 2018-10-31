b94fc0b215cc11e8d338bac984717cf9185ee3b2
ChristianBD <44215681+ChristianBD@users.noreply.github.com>
2018-10-31 07:08:08 -0400

    Added Timeout functionnality, switched delays to time readings

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -3,54 +3,96 @@
   */
   
   #define ledPin PC13
+  int LEDSTATUS = 0;
   char incomingByte = 0;   // for incoming serial data
   short s = 0;
-  bool START;
-  bool STOP;
+  String buffer = "";
+  bool fired = false;
+  
+  unsigned long previousPacketMillis = 0;
+  unsigned long previousBlinkMillis = 0;
+  unsigned long currentMillis;
+  unsigned long Ping;
+
+  const int Packetinterval = 100; //ms to send packets
+  const int TimeOut = 2000; //ms to time out and force stop engine
+  const int TimeOutBlinktime = 200;
+  const int IddleBlinkTime = 1000;
+  int blinktime = IddleBlinkTime;
+  char RocketStatus; // F = fired, X= Emergency stop, I = Iddle, D = Disconnected, x= stopped because disconnect while firing
   
   // the setup function runs once when you press reset or power the board
   void setup() {
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, HIGH);   
     Serial1.begin(9600);
-    START = false;
-    STOP = false;
-
+    RocketStatus = 'I';
   }
 
-   
   // the loop function runs over and over again forever
   void loop() {
     
- 
-  
-  if (!STOP){
               if (Serial1.available()) {
                     incomingByte = Serial1.read();
               }
-          //    delay(10);
                   // read the incoming byte:
-                  if (incomingByte=='S'){
-                  START = true;
-                      digitalWrite(ledPin, LOW);  
+                  if (incomingByte=='S' &&  RocketStatus == 'I'){
+                  RocketStatus = 'F';
+                      digitalWrite(ledPin, LOW);
+                      fired = true;  
                     } else if (incomingByte=='X'){
-                  STOP = true;
-              digitalWrite(ledPin, HIGH);   
+                  RocketStatus = 'X';
+              digitalWrite(ledPin, HIGH);
+              fired =true;   
+              } else if (incomingByte=='P'){ // Ping from Android
+               Ping  = millis();
               }
-              
-  if(START){
-  
-  s = random (0,256);
-  Serial1.print(s,HEX);
-    for (int i=0;i<28;i++){
-      s = random (0,256);
-      Serial1.print("-");
-      Serial1.print(s,HEX);
+
+//Bluetooth Connection Detector
+  currentMillis = millis();
+  if (currentMillis - Ping >= TimeOut) { // if timeout
+
+        if (fired){ // If fired, abort
+    RocketStatus = 'x';
+    blinktime = TimeOutBlinktime;
+    }
+    if (!fired){ // if not already emergency stopped
+    RocketStatus = 'D'; // Set disconnect
+    }
+
+    
+    if (currentMillis - previousBlinkMillis >= blinktime) {
+    previousBlinkMillis = currentMillis;
+
+    
+    // if the LED is off turn it on and vice-versa:
+      if (LEDSTATUS) {
+      digitalWrite(ledPin, LOW);
+      LEDSTATUS = !LEDSTATUS;
+      } else {
+      digitalWrite(ledPin, HIGH);
+      LEDSTATUS = !LEDSTATUS;
       }
-       Serial1.print("\n");
-  
-   delay(100); // wait for 0.1 second
-   
-        }
-      } 
+    }
+  }else if(!fired){
+   digitalWrite(ledPin, HIGH);
+   RocketStatus = 'I';
+  }
+              
+              
+   if (currentMillis - previousPacketMillis >= Packetinterval) {
+   previousPacketMillis = currentMillis;
+buffer = "";
+
+buffer += String(RocketStatus);
+    for (int i=0;i<29;i++){
+      buffer +="-";
+      s = random (0,256);
+      buffer += String(s,HEX);
+      }
+buffer += "\n";
+
+Serial1.print(buffer);
+         }
+      
    }
