e0016f2b64788beb226e61e0e0097f7ac4be1b42
John Turpin <johnturpin369@gmail.com>
2018-11-09 11:12:30 -0500

    Fixed bug where user could not scroll through the list of sensors. Added a sensor subclass that I will likely use to help with graphing data later.

---
+++ app/src/main/java/com/example/spaceconcordia/spacecadets/Data_Types/Sensor.java
@@ -0,0 +1,21 @@
+package com.example.spaceconcordia.spacecadets.Data_Types;
+
+import java.io.Serializable;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Sensor implements Serializable {
+
+    private String name;
+    private short value;
+
+    private BlockingQueue sensorDataQueue = new LinkedBlockingQueue(100);
+
+    public Sensor(){}
+
+    public String getName(){return name;}
+    public short getValue(){return value;}
+
+
+
+}
