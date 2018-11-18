632554a4427d8176486676a8875cff5f97024804
Christian BD <chrisbdmc@gmail.com>
2018-11-18 13:50:40 -0500

    - Completly Reimplemented Database

---
+++ app/src/main/java/com/example/spaceconcordia/spacecadets/Database/DBConfig.java
@@ -0,0 +1,41 @@
+package com.example.spaceconcordia.spacecadets.Database;
+
+public class DBConfig {
+
+    public static final String DATABASE_NAME = "Telemetry-db";
+
+    //column names of student table
+    public static final String TABLE_TELEMETRY = "Telemetry";
+    public static final String COLUMN_ID = "_id";
+    public static final String Time_ms = "Time_MS";
+    public static final String Status = "Status";
+    public static final String sens1 = "Temp1";
+    public static final String sens2 = "Temp2";
+    public static final String sens3 = "Temp3";
+    public static final String sens4 = "Temp4";
+    public static final String sens5 = "Temp5";
+    public static final String sens6 = "Temp6";
+    public static final String sens7 = "Temp7";
+    public static final String sens8 = "Temp8";
+    public static final String sens9 = "Temp9";
+    public static final String sens10 = "Temp10";
+    public static final String sens11 = "Temp11";
+    public static final String sens12 = "Temp12";
+    public static final String sens13 = "Temp13";
+    public static final String sens14 = "Temp14";
+    public static final String sens15 = "flow1";
+    public static final String sens16 = "flow2";
+    public static final String sens17 = "press1";
+    public static final String sens18 = "press2";
+    public static final String sens19 = "press3";
+    public static final String sens20 = "press4";
+    public static final String sens21 = "press5";
+    public static final String sens22 = "press6";
+    public static final String sens23 = "press7";
+    public static final String sens24 = "press8";
+    public static final String sens25 = "press9";
+    public static final String sens26 = "press10";
+    public static final String sens27 = "press11";
+    public static final String sens28 = "press12";
+    public static final String sens29 = "press13";
+}
